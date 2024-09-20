cc7c4d8e971c9d92222fd4ade8683f8cbef5a931
Julio Cesar Balbino de Morais <julio4110@hotmail.com>
2024-09-20 15:02:27 -0300

    update: show_images_carousel

--- qrcodeaux.py
+++ qrcodeaux.py
@@ -1,7 +1,6 @@
 import qrcode
 import io
 
-
 def generate_qr_code(data):
     qr = qrcode.QRCode(
         version=1,
@@ -12,12 +11,16 @@ def generate_qr_code(data):
     qr.add_data(data)
     qr.make(fit=True)
 
-    img = qr.make_image(fill_color="black", back_color="white")
+    img = qr.make_image(fill_color="black", back_color="white").convert('RGB')
+
+    pixels = img.load()
+    for y in range(img.size[1]):
+        for x in range(img.size[0]):
+            if pixels[x, y] == (255, 255, 255):
+                pixels[x, y] = (227, 217, 185)
 
     img_bytes = io.BytesIO()
-    img.save(img_bytes)
+    img.save(img_bytes, format='PNG')
     img_bytes.seek(0)
 
     return img_bytes
-
-
